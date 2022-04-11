d9be942728cdfcace23980cf736b735d3e0aa457
nmpeterson <nmpeterson@users.noreply.github.com>
2022-04-11 12:37:16 -0500

    Precompute distances between geographies

--- data-raw/geolist.R
+++ data-raw/geolist.R
@@ -1,8 +1,9 @@
 #install.packages(c("tidyverse", "sf"))
 #devtools::install_github('cmap-repos/cmapgeo')
-library(cmapgeo)
 library(tidyverse)
 library(sf)
+library(cmapgeo)
+
 
 ## Coerce desired geographies (municipalities, CCAs and/or counties) into a unified schema.
 ## (Note that each will need to have a unique name, or else app.R could have problems.
@@ -20,7 +21,7 @@ munis <- municipality_sf %>%
 #          geo_name = paste(county, "County"),
 #          geo_name_std = tolower(geo_name)) %>%
 #   select(geo_type, geo_name, geo_name_std)
-#
+
 # ccas <- cca_sf %>%
 #   filter(cca_name != "Riverdale") %>%  # Avoid confusion with the muni of the same name
 #   mutate(geo_type = "CCA",
@@ -33,5 +34,11 @@ munis <- municipality_sf %>%
 all_geos <- munis %>%
   mutate(centroid = st_centroid(.$geometry))
 
-## Save dataset for importing in app.R
+
+## Pre-calculate distances between all geographies
+geo_distances <- st_distance(all_geos, all_geos)
+
+
+## Save datasets for importing in app.R
 save(all_geos, file="data/all_geos.rda")
+save(geo_distances, file="data/geo_distances.rda")
