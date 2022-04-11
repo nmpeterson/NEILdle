b0d3deb53ba986c6cc72af0e11889cd913d8f20f
nmpeterson <nmpeterson@users.noreply.github.com>
2022-04-11 11:08:07 -0500

    Initial code for NEILdle
    Forked wch/shiny-wordle as the basis for a Northeastern Illinois (NEIL)-focused Worldle knockoff, run as a Shiny app

---
+++ data-raw/geolist.R
@@ -0,0 +1,37 @@
+#install.packages(c("tidyverse", "sf"))
+#devtools::install_github('cmap-repos/cmapgeo')
+library(cmapgeo)
+library(tidyverse)
+library(sf)
+
+## Coerce desired geographies (municipalities, CCAs and/or counties) into a unified schema.
+## (Note that each will need to have a unique name, or else app.R could have problems.
+## For example, McHenry (muni) and McHenry (county) need to be differentiated, as do
+## Riverdale (muni) and Riverdale (CCA).)
+munis <- municipality_sf %>%
+  mutate(geo_type = "municipality",
+         geo_name = str_replace_all(municipality, "['.]", ""),
+         geo_name_std = tolower(geo_name)) %>%
+  select(geo_type, geo_name, geo_name_std)
+
+# counties <- county_sf %>%
+#   filter(cmap) %>%
+#   mutate(geo_type = "county",
+#          geo_name = paste(county, "County"),
+#          geo_name_std = tolower(geo_name)) %>%
+#   select(geo_type, geo_name, geo_name_std)
+#
+# ccas <- cca_sf %>%
+#   filter(cca_name != "Riverdale") %>%  # Avoid confusion with the muni of the same name
+#   mutate(geo_type = "CCA",
+#          geo_name = str_replace_all(cca_name, "['.]", ""),
+#          geo_name_std = tolower(geo_name)) %>%
+#   select(geo_type, geo_name, geo_name_std)
+
+## Combine any geographies into a single table and create centroid points
+# all_geos <- bind_rows(counties, munis, ccas) %>%
+all_geos <- munis %>%
+  mutate(centroid = st_centroid(.$geometry))
+
+## Save dataset for importing in app.R
+save(all_geos, file="data/all_geos.rda")
